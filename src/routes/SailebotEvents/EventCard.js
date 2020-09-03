91582cb013bc94812b3dd522fbf41cdf7a8d6af5
Clive R. Cadogan <ricardo_0x07@yahoo.com>
2020-09-03 19:49:09 -0400

    added to clarify

--- src/routes/SailebotEvents/EventCard.js
+++ src/routes/SailebotEvents/EventCard.js
@@ -6,6 +6,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 import Moment from 'react-moment';
+import { updateEvent } from "../../graphql/mutations";
+import { adopt } from 'react-adopt';
+import { Mutation } from "react-apollo";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,7 +18,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export const EventCard = ({ event }) => {
+export const EventCard = ({ event, updateReload }) => {
   const classes = useStyles();
   const [state, setState] = React.useState({
     showBody: false,
@@ -47,7 +50,54 @@ export const EventCard = ({ event }) => {
   //     </React.Fragment>
   //   );
   // }
-
+  const dismissClarification = (updateEventMutation) => async () => {
+    const {
+      cc,
+      date,
+      id,
+      label,
+      sender,
+      subject,
+      body,
+      contact_id,
+      nlu_input_text,
+      nlu_json_response,
+      selected_intent,
+      validated_json_response,
+      validated_intent,
+      campaign_id,
+      is_inbound,
+      to_clarify,
+      to,
+    } = event;
+    
+    const toClarify=true
+    await updateEventMutation({
+      variables: {
+          objects: {
+            cc,
+            date,
+            id,
+            label,
+            sender,
+            subject,
+            body,
+            contact_id,
+            nlu_input_text,
+            nlu_json_response,
+            selected_intent,
+            validated_json_response,
+            validated_intent,
+            campaign_id,
+            is_inbound,
+            to_clarify: toClarify,
+            to,
+          },
+          id
+      }
+    });
+    updateReload()
+  }
   const insertBody = (body) => {
     return (
       <React.Fragment>
@@ -58,24 +108,37 @@ export const EventCard = ({ event }) => {
       </React.Fragment>
     );
   }
-
+  const Composed = adopt({
+    updateEventMutation: ({ render }) => (
+        <Mutation mutation={ updateEvent } >
+          { render }
+        </Mutation> 
+    ),
+  })
   // console.log(body.split("\n").slice(0,4))
   return (
-    <Card>
-      <CardContent>
-        <Typography><strong>Label: </strong>{label} <strong>From:</strong> {sender} <strong>To:</strong> {to}</Typography>
-        <Typography><strong>Subject:</strong> {subject} <strong>Cc:</strong> {cc}</Typography>
-        <Typography>
-          <strong>Date:</strong> <Moment format="YYYY-MMM-DD" date={date !== null && date }></Moment>
-        </Typography>
-        <CardActions className={classes.root}>
-          <Button variant="contained" size="small" onClick={handleChange}>{!state.showBody ? "View Body" : "Hide Body"}</Button>
-        </CardActions>
-        {
-          state.showBody &&
-          insertBody(body)
-        }
-      </CardContent>
-    </Card>
+    <Composed>
+      {({ updateEventMutation  }) => {
+        return (
+          <Card>
+            <CardContent>
+              <Typography><strong>Label: </strong>{label} <strong>From:</strong> {sender} <strong>To:</strong> {to}</Typography>
+              <Typography><strong>Subject:</strong> {subject} <strong>Cc:</strong> {cc} <strong>Date:</strong> <Moment format="YYYY-MMM-DD" date={date !== null && date }></Moment></Typography>
+              {/* <Typography>
+                
+              </Typography> */}
+              <CardActions className={classes.root}>
+                <Button variant="contained" size="small" onClick={handleChange}>{!state.showBody ? "View Body" : "Hide Body"}</Button>
+                <Button variant="contained" size="small" onClick={dismissClarification(updateEventMutation)}>To clarify</Button>
+              </CardActions>
+              {
+                state.showBody &&
+                insertBody(body)
+              }
+            </CardContent>
+          </Card>    
+        );
+      }}
+    </Composed>
   );
 };
